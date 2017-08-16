e281baff7cc7bd98fb89522e5c13c6dee8a2aee9
goldragon <goldragon227@gmail.com>
2017-08-16 09:30:28 -0700

    Add func class and Sum function

---
+++ 5-Multi-FileProjects/func.cpp
@@ -0,0 +1,6 @@
+#include "func.h"
+
+int Sum(int a, int b)
+{
+	return a + b;
+}
--- 5-Multi-FileProjects/main.cpp
+++ 5-Multi-FileProjects/main.cpp
@@ -1,4 +1,10 @@
-void main()
-{
+#include <iostream>
 
+#include "func.h"
+
+int main()
+{
+	std::cout << Sum(6, 7);
+	system("PAUSE");
+	return 0;
 }
