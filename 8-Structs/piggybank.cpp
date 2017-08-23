41e645ced306eecaa177e80d9e6e68d4669ea54d
goldragon <goldragon227@gmail.com>
2017-08-22 17:18:19 -0700

    Edit piggy

--- 8-Structs/piggybank.cpp
+++ 8-Structs/piggybank.cpp
@@ -11,8 +11,8 @@ float calcPiggybankCoins(piggybank piggy)
 {
 	return piggy.numPennies * 0.01f +
 		   piggy.numNickels * 0.05f +
-		   piggy.numDimes * 0.1f	+
-		   piggy.numQuarters * 0.25f;
+		   piggy.numDimes	* 0.1f	+
+		   piggy.numQuarters* 0.25f;
 }
 
 // Returns the total monetary value of the piggybank's contents.
